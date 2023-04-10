8adb54aa0fe9c4276c0bdf7c34326e0fc072040f
soooooooora <127271130+soooooooora@users.noreply.github.com>
2023-04-10 19:17:01 +0900

    商品出品機能

---
+++ app/models/item.rb
@@ -0,0 +1,32 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :prefecture
+  belongs_to :item_status
+  belongs_to :shipping_cost
+  belongs_to :shipping_date
+
+  with_options presence: true do
+    validates :user_id
+    validates :image
+    validates :name
+    validates :description
+    validates :category_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :prefecture_id
+    validates :shipping_date_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :prefecture_id
+    validates :item_status_id
+    validates :shipping_cost_id
+    validates :shipping_date_id
+  end
+end
