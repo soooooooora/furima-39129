7336372c12937f2310e0c119c16c7ea4b5863024
soooooooora <127271130+soooooooora@users.noreply.github.com>
2023-04-12 18:06:20 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -33,7 +33,7 @@ class ItemsController < ApplicationController
     if @item.update(item_params)
       redirect_to item_path(item_params)
     else
-      render 'edit'
+      render :edit
     end
   end
 
